d865a3db83ead23fe7240029de236c870b35c561
Matt <matt_campbell@hotmail.co.uk>
2024-03-01 11:06:21 +0700

    completed portfolio and respinsive

---
+++ java.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var images = document.querySelectorAll('.enlarge-image');
+  
+    images.forEach(function(image) {
+      image.addEventListener('click', function() {
+        image.classList.toggle('enlarged');
+      });
+    });
+  });
